dd71382d7cd6932833275f5956a694b236296c59
Sangine <damien.girard1305@gmail.com>
2017-12-19 15:46:36 +0100

    Exercise 2

---
+++ src/exercise2.js
@@ -0,0 +1,10 @@
+exports.run = function(input) {
+        var res = input.split(",");
+        var somme = 0;
+        for (var i=0; i < res.length; i++) {
+            somme += Number(res[i]);
+        }
+        console.log(somme);
+        return somme;
+};
+
